86708b0f1c4dd2a5bff10425b0d631c127f60d73
Real Guay <herge980@hotmail.com>
2023-06-06 17:46:10 -0400

    Remove playing feature from segment editor.

--- MusicBox/Modules/MusicBox.Modules.SheetEditor/ViewModels/SegmentEditorViewModel.cs
+++ MusicBox/Modules/MusicBox.Modules.SheetEditor/ViewModels/SegmentEditorViewModel.cs
@@ -15,7 +15,6 @@ namespace MusicBox.Modules.SheetEditor.ViewModels
         private const string DefaultName = "Segment1";
         private readonly IEventAggregator _eventAggregator;
         private readonly Func<TimeSignature, KeySignature, IBarEditorViewModel> _barEditorVmCreator;
-        private readonly IMidiPlayer _midiPlayer;
         private readonly Segment _segment;
         private string segmentName;
 
@@ -35,39 +34,23 @@ namespace MusicBox.Modules.SheetEditor.ViewModels
         public List<KeySignature> KeySignatures { get; private set; }
 
         private KeySignature selectedKeySignature;
-        public KeySignature SelectedKeySignature { get => selectedKeySignature; set => SetProperty(ref selectedKeySignature , value); }
-
-        private bool isPlaying;
-        public bool IsPlaying { get => isPlaying; set => SetProperty(ref isPlaying, value); }
-
-        private int tempo;
-        public int Tempo { get => tempo; set => SetProperty(ref tempo, value); }
+        public KeySignature SelectedKeySignature { get => selectedKeySignature; set => SetProperty(ref selectedKeySignature, value); }
 
         public bool IsSegmentEmpty { get => BarEditorVms.Count == 0; }
 
         public DelegateCommand AddBarCommand { get; set; }
         public DelegateCommand DeleteBarCommand { get; set; }
-        public DelegateCommand PlayCommand { get; set; }
-        public DelegateCommand PauseCommand { get; set; }
-        public DelegateCommand RewindToZeroCommand { get; set; }
 
-        public SegmentEditorViewModel(Func<TimeSignature, KeySignature, IBarEditorViewModel> barEditorVmCreator, IMidiPlayer midiPlayer, IEventAggregator evenAggregator)
+        public SegmentEditorViewModel(Func<TimeSignature, KeySignature, IBarEditorViewModel> barEditorVmCreator, IEventAggregator evenAggregator)
         {
             _barEditorVmCreator = barEditorVmCreator;
-            _midiPlayer = midiPlayer;
             _eventAggregator = evenAggregator;
             SegmentName = DefaultName;
             BarEditorVms = new ObservableCollection<IBarEditorViewModel>();
             AddBarCommand = new DelegateCommand(AddBar);
             DeleteBarCommand = new DelegateCommand(DeleteBar, CanDeleteBar).ObservesProperty(() => SelectedBarEditorVm);
-            PlayCommand = new DelegateCommand(Play, CanPlay).ObservesProperty(() => IsPlaying);
-            PauseCommand = new DelegateCommand(Pause, CanPause).ObservesProperty(() => IsPlaying);
-            RewindToZeroCommand = new DelegateCommand(RewindToZero, CanRewindToZero).ObservesProperty(() => IsPlaying); ;
-            Tempo = 60;
             _eventAggregator.GetEvent<SelectedBarChanged>().Subscribe(OnSelectedBarChanged);
             _segment = new Segment();
-            _midiPlayer.PlayingState += _midiPlayer_PlayingState;
-            IsPlaying = false;
             InitSignatures();
         }
 
@@ -77,42 +60,6 @@ namespace MusicBox.Modules.SheetEditor.ViewModels
             SelectedKeySignature = KeySignature.Natural;
             TimeSignatures = TimeSignature.AllTimeSignatures;
             SelectedTimeSignature = TimeSignature.TS_4_4;
-        }
-
-        private void _midiPlayer_PlayingState(bool isPlaying)
-        {
-            IsPlaying = isPlaying;
-        }
-
-        private void Play()
-        {
-            ExtractSegmentInfo(_segment, PlayingHand.Both);
-            _midiPlayer.PlaySegment(_segment, Tempo);
-        }
-
-        private bool CanPlay()
-        {
-            return !IsPlaying && BarEditorVms.Count > 0;
-        }
-
-        private void Pause()
-        {
-            _midiPlayer.Pause();
-        }
-
-        private bool CanPause()
-        {
-            return IsPlaying;
-        }
-
-        private void RewindToZero()
-        {
-            _midiPlayer.RewindToZero();
-        }
-
-        private bool CanRewindToZero()
-        {
-            return !IsPlaying && BarEditorVms.Count > 0;
         }
 
         private void OnSelectedBarChanged(IBarEditorViewModel newSelectedBarVm)
@@ -128,7 +75,6 @@ namespace MusicBox.Modules.SheetEditor.ViewModels
             var newBarEditorVm = _barEditorVmCreator(SelectedTimeSignature, SelectedKeySignature);
             BarEditorVms.Add(newBarEditorVm);
             SelectedBarEditorVm = newBarEditorVm;
-            PlayCommand.RaiseCanExecuteChanged();
             RaisePropertyChanged(nameof(IsSegmentEmpty));
         }
 
@@ -154,7 +100,7 @@ namespace MusicBox.Modules.SheetEditor.ViewModels
 
         public ISegmentEditorViewModel DeepCopy()
         {
-            SegmentEditorViewModel copy = new SegmentEditorViewModel(_barEditorVmCreator, _midiPlayer, _eventAggregator)
+            SegmentEditorViewModel copy = new SegmentEditorViewModel(_barEditorVmCreator, _eventAggregator)
             {
                 SegmentName = SegmentName
             };
@@ -201,16 +147,15 @@ namespace MusicBox.Modules.SheetEditor.ViewModels
         {
             SegmentName = segment.Name;
             SelectedTimeSignature = segment.TimeSignature;
-            SelectedKeySignature = segment.KeySignature;    
+            SelectedKeySignature = segment.KeySignature;
 
             foreach (var bar in segment.Bars)
             {
-                var newBarEditorVm = _barEditorVmCreator(SelectedTimeSignature, SelectedKeySignature); 
+                var newBarEditorVm = _barEditorVmCreator(SelectedTimeSignature, SelectedKeySignature);
                 newBarEditorVm.LoadBarInfo(bar);
                 BarEditorVms.Add(newBarEditorVm);
             }
             SelectedBarEditorVm = BarEditorVms.Count > 0 ? BarEditorVms[0] : null;
         }
-
     }
 }
